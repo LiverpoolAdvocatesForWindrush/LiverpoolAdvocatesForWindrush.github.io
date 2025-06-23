e266e1a376960189d6dad372fb96f6defcc5495b
lydiaankers <lydiaankers14@gmail.com>
2025-06-23 19:21:48 +0100

    name fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/Eligibility-Website/",
+    base: "/LiverpoolAdvocatesForWindrush.github.io/",
   optimizeDeps: {
     exclude: ['lucide-react'],
     },
